1274ec7fc0b0716c93057997ffc4b0270ffe34a0
ductt <ductt@fabbi.io>
2021-03-03 10:55:42 +0700

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import { useDarkMode } from "./ultils/darkMode"
+import Navbar from "./components/Navbar"
+
 
 function App() {
+  const [theme, toogleTheme] = useDarkMode();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${theme}`} >
+      <Navbar />
+      <button onClick={toogleTheme}>{theme}</button>
+
     </div>
   );
 }
-
-export default App;
+export default App
